136bc78ec53334935ce33e8e03f7f2c48c0c3c1d
rh <mago1291@naver.com>
2024-04-15 15:06:02 +0900

    Add mount option

--- docker/for_api/run_api.sh
+++ docker/for_api/run_api.sh
@@ -6,6 +6,7 @@ docker run \
     --name $API_CONTAINER \
     --hostname $API_CONTAINER \
     -p $API_PORT:$API_PORT \
+    -v $PROJECT_PATH/.env:/.env \
     -v $PROJECT_PATH/server:/server \
     -v $PROJECT_PATH/docker/for_api:/docker \
     -ti $API_IMAGE \
